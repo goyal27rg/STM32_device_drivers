9a748fdf6310b9affbcb63a4719c38096152c34e
goyal27rg <rakshit.255111@gmail.com>
2022-09-07 21:51:07 +0530

    I2C: Adding basic driver .h and .c files and some macros to main stm32f29xx.h

---
+++ STM32/stm32_drivers/drivers/Src/stm32f429xx_i2c_driver.c
@@ -0,0 +1,17 @@
+/*
+ * stm32f429xx_i2c_driver.c
+ *
+ *  Created on: 04-Sep-2022
+ *      Author: rakshit
+ */
+
+/*
+ * 1. I2C Init
+ * 2. I2C Master Tx
+ * 3. I2C Master Rx
+ * 4. I2C Slave Tx
+ * 5. I2C Slave Rx
+ * 6. I2C Error Interrupt handling
+ * 7. I2C Event Interrupt handling
+ */
+
